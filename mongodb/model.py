4ceb6e33b67792620e482ecc338343e555e03430
Ed Me <603803799@qq.com>
2018-01-29 18:13:14 +0800

    modify readme.md

--- mongodb/model.py
+++ mongodb/model.py
@@ -14,27 +14,28 @@ print(_v['test'])
 
 class user:  # 用户
     def __init__(self, dict={}):
-        name = self.__class__.__name__
-        if (not _v.test(dict, _s[name]())):
+        if (not _v.test(dict, _s[self.__class__.__name__]())):  # TODO:多条插入需要逐步循环验证
             raise ValueError('{}：参数类型错误'.format(name))
         self.data = dict
         # print(self.__class__.__name__)
-        self.db = db.name  # 使用user集合，没有则自动创建
+        self.db = db[self.__class__.__name__]  # 使用user集合，没有则自动创建
+        return
 
-    def insert(self):  # 插入
-        pass
+    def insert(self, dict):  # 插入
+        # self.db.save(self.data)
+        return self.db.insert(dict)
 
-    def findOne(self):  # 单个查找
-        pass
+    def findOne(self, dict):  # 单个查找
+        return self.db.find_one(dict)
 
-    def find(self):  # 查询
-        pass
+    def find(self, dict, skip=1 * 15, limit=15):  # 查询
+        return self.db.find(dict).skip(skip).limit(limit)
 
-    def remove(self):  # 移除
-        pass
+    def remove(self, dict):  # 移除
+        return self.db.remove(dict)
 
-    def update(self):  # 修改
-        pass
+    def update(self, dict, data):  # 修改
+        return self.db.update(dict, data)
 
 
 # user()
@@ -45,20 +46,21 @@ class label:  # 文章标签/分类
         self.data = dict
         self.db = db[self.__class__.__name__]  # 使用label集合，没有则自动创建
 
-    def insert(self):  # 插入
-        pass
+    def insert(self, dict):  # 插入
+        # self.db.save(self.data)
+        return self.db.insert(dict)
 
-    def findOne(self):  # 单个查找
-        pass
+    def findOne(self, dict):  # 单个查找
+        return self.db.find_one(dict)
 
-    def find(self):  # 查询
-        pass
+    def find(self, dict, skip=1 * 15, limit=15):  # 查询
+        return self.db.find(dict).skip(skip).limit(limit)
 
-    def remove(self):  # 移除
-        pass
+    def remove(self, dict):  # 移除
+        return self.db.remove(dict)
 
-    def update(self):  # 修改
-        pass
+    def update(self, dict, data):  # 修改
+        return self.db.update(dict, data)
 
 
 class commet:  # 文章评论
@@ -66,20 +68,21 @@ class commet:  # 文章评论
         self.data = dict
         self.db = db[self.__class__.__name__]  # 使用commet集合，没有则自动创建
 
-    def insert(self):  # 插入
-        pass
+    def insert(self, dict):  # 插入
+        # self.db.save(self.data)
+        return self.db.insert(dict)
 
-    def findOne(self):  # 单个查找
-        pass
+    def findOne(self, dict):  # 单个查找
+        return self.db.find_one(dict)
 
-    def find(self):  # 查询
-        pass
+    def find(self, dict, skip=1 * 15, limit=15):  # 查询
+        return self.db.find(dict).skip(skip).limit(limit)
 
-    def remove(self):  # 移除
-        pass
+    def remove(self, dict):  # 移除
+        return self.db.remove(dict)
 
-    def update(self):  # 修改
-        pass
+    def update(self, dict, data):  # 修改
+        return self.db.update(dict, data)
 
 
 class article:  # 文章列表
@@ -87,17 +90,18 @@ class article:  # 文章列表
         self.data = dict
         self.db = db[self.__class__.__name__]  # 使用article集合，没有则自动创建
 
-    def insert(self):  # 插入
-        pass
+    def insert(self, dict):  # 插入
+        # self.db.save(self.data)
+        return self.db.insert(dict)
 
-    def findOne(self):  # 单个查找
-        pass
+    def findOne(self, dict):  # 单个查找
+        return self.db.find_one(dict)
 
-    def find(self):  # 查询
-        pass
+    def find(self, dict, skip=1 * 15, limit=15):  # 查询
+        return self.db.find(dict).skip(skip).limit(limit)
 
-    def remove(self):  # 移除
-        pass
+    def remove(self, dict):  # 移除
+        return self.db.remove(dict)
 
-    def update(self):  # 修改
-        pass
+    def update(self, dict, data):  # 修改
+        return self.db.update(dict, data)
